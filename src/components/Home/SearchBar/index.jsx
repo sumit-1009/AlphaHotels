087b804c1027b9eb7aff62d63e83e99f4f815a68
Sumit Sahu <sssahu1009@gmail.com>
2022-08-09 23:28:39 +0530

    Project Done

--- src/components/Home/SearchBar/index.jsx
+++ src/components/Home/SearchBar/index.jsx
@@ -7,7 +7,7 @@ const SearchBar = ({ value, changeInput }) => (
     <SearchIcon className='searchBar-icon' />
     <input
       type='text'
-      placeholder='Woodland Hills'
+      placeholder='AlphaHotels'
       value={value}
       onChange={changeInput}
     />
